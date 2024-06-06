4eafe785ec9ed226c700b9c2ca2231e4ed5a44fb
Cesar Fajardo <cesarjavierfajardoortiz@gmail.com>
2024-06-05 22:04:15 -0500

    MVP Actualizado en Logica de Login

--- js/login.js
+++ js/login.js
@@ -1,23 +1,28 @@
-const EMPTY_STRING = "";
+// CONSTANTS
 const CREDENTIALS = Object.freeze({ USERNAME: "Cesar4280", PASSWORD: "WebDesign2020" });
-
+// DOM Elements
 const loginForm = document.forms.namedItem("login");
-const loginInputs = [...loginForm.elements].toSpliced(2, 1);
+const loginInputs = [...loginForm.elements].slice(0, 2);
 const loginButtonLink = loginForm.querySelector("a");
 console.log(loginInputs, loginButtonLink);
-
+// UTILS
+const isBlankOrEmptyText = ({ value }) => !value.trim(); // "" => true o "xxx" => false
+const showInputErrorMessage = ({ id }) => alert(`Complete el campo ${id === "username" ? "usuario" : "contraseña"}`);
+const isFailedCredential = input => input.value !== CREDENTIALS[input.id.toUpperCase()];
+const reportInvalidFields = inputs => inputs.forEach(input => isBlankOrEmptyText(input) && showInputErrorMessage(input));
+// EVENT HANDLERS
+const handleClickSignIn = event => checkLoginForm(loginInputs) || event.preventDefault();
+// LISTEN EVENTS
 loginButtonLink.addEventListener("click", handleClickSignIn);
-
-function handleClickSignIn(event) {
-
-    if (loginInputs.some(({ value }) => value.trim() === EMPTY_STRING)) {
-        loginInputs.forEach(input => input.value.trim() === EMPTY_STRING &&
-            alert(`Complete el campo ${input.id === "username" ? "usuario" : "contraseña"}`)
-        );
-        event.preventDefault();
-    }
-    else if (loginInputs.some(input => input.value !== CREDENTIALS[input.id.toUpperCase()])) {
+// VALIDATION FUNCTION
+function checkLoginForm(inputs) {
+    let canNavigate = true;
+    if (inputs.some(isBlankOrEmptyText)) {
+        reportInvalidFields(inputs);
+        canNavigate = false;
+    } else if (inputs.some(isFailedCredential)) {
         alert("Nombre de Usuario o Contraseña Incorrectas");
-        event.preventDefault();
+        canNavigate = false;
     }
+    return canNavigate;
 }
